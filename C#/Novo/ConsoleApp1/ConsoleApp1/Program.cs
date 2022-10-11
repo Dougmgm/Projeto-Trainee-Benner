19f4883d9ad5307fe26fa685cecca0ee9d2e49d3
Doumgm <114234741+Doumgm@users.noreply.github.com>
2022-10-11 15:46:18 -0300

    Add files via upload

---
+++ C#/Novo/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Qual o seu Nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Qual a sua idade?");
+            string idade = Console.ReadLine();
+            Console.WriteLine("Qual o seu hobby favorito?");
+            string hobbie = Console.ReadLine();
+            Console.WriteLine($"Olá: {nome}, você tem {idade} anos. Você gosta de {hobbie}");*/
+
+            /* int x = 5;
+             int y = 19;
+             int z = x + y;
+             Console.WriteLine(z);*/
+
+            /*int numeroUm, numeroDois;
+
+            Console.WriteLine("Escolha um número inteiro:");
+            numeroUm = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escolha outro número inteiro:");
+            numeroDois = Convert.ToInt32(Console.ReadLine());
+
+            int soma = numeroUm + numeroDois;
+            Console.WriteLine(soma);*/
+
+            // print the output of (x+y).z and x.y + y.z.
+
+            /* int numeroUm, numeroDois, numeroTres;
+
+             Console.WriteLine("Escolha um número inteiro:");
+             numeroUm = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine("Escolha outro número inteiro:");
+             numeroDois = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine("Escolha mais um número inteiro:");
+             numeroTres = Convert.ToInt32(Console.ReadLine());
+
+             int calculo1 = (numeroUm + numeroDois) * numeroTres;
+             Console.WriteLine(calculo1);*/
+
+            /* int numeroUm, numeroDois, numeroTres;
+
+             Console.WriteLine("Escolha um número inteiro:");
+             numeroUm = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine("Escolha outro número inteiro:");
+             numeroDois = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine("Escolha mais um número inteiro:");
+             numeroTres = Convert.ToInt32(Console.ReadLine());
+
+             int calculo1 = (numeroUm * numeroDois) + (numeroUm * numeroTres);
+             Console.WriteLine(calculo1);*/
+
+
+            double x = 5.3;
+            double y = 5.6;
+            Console.WriteLine(Math.Round(x));
+            Console.WriteLine(Math.Round(y));
+        }
+    }
+}
+        
+
+
+//VARIAVEL GUARDA VALOR
+//identifier: é o que chamamos de variável 
