68b060316b073acf73a2f76b8ec860bf2d2b5c4a
Douglas Menchon <114234741+Dougmgm@users.noreply.github.com>
2022-10-26 15:25:00 -0300

    Update loginscript.js

--- Desafio JS/script/loginscript.js
+++ Desafio JS/script/loginscript.js
@@ -1,11 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
 
-window.onload=function(){
-    const botao = document.getElementById('loginEntrada');
-    botao.addEventListener('click', function(){
-        if(loginUsuario === "Marcelo" && loginSenha === "senha"){
-            alert("senha ok");
-        } else {
-            alert("not ok");
+    document.querySelectorAll('.sair').forEach(function(button){
+
+        button.onclick = function(){
+
+            document.querySelectorAll('.sair');
+            alert('teste');
+            console.log('teste');
         }
     })
-}
+});
+
