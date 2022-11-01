ea5fcce39f959a803d9bfbf631bdd86d34b9bbe0
Douglas Menchon <114234741+Dougmgm@users.noreply.github.com>
2022-11-01 10:06:41 -0300

    Update loginscript.js

--- Desafio JS/script/loginscript.js
+++ Desafio JS/script/loginscript.js
@@ -4,11 +4,17 @@ import { produtos } from "/HTML/Desafio/module/produtos.js"
 const botaoEsquerda = document.querySelectorAll('.esquerda');
 const botaoSair = document.querySelectorAll('.sair');
 const botaoMov = document.querySelectorAll('.btnAjuste');
+const botaoNovo = document.querySelectorAll('.btnAjuste');
+let posicaoCliente = 1
+
+TransicaoCliente(1);
+TransicaoProdutos(1);
 
 for(let botao of botaoEsquerda){    
     botao.addEventListener('click', (event) => {
 
         if(event.target.id == 'primeiro'){
+            3
             if(document.forms[0].style.display == 'block'){
                 document.forms[0].style.display = 'none';
                 document.forms[1].style.display = 'none';
@@ -18,7 +24,7 @@ for(let botao of botaoEsquerda){
                 document.forms[0].style.display = 'block'
                 document.forms[1].style.display = 'none';
                 document.forms[2].style.display = 'none';
-                TransicaoCliente(1);
+                
             }
         } else if(event.target.id =='segundo'){
             if(document.forms[1].style.display == 'block'){
@@ -26,11 +32,11 @@ for(let botao of botaoEsquerda){
                 document.forms[0].style.display = 'none';
                 document.forms[2].style.display = 'none';
             }
-            else{
+            else{   
                 document.forms[1].style.display = 'block';
                 document.forms[0].style.display = 'none';
                 document.forms[2].style.display = 'none';
-                TransicaoProdutos(1);
+                
             }
         } else{
             if(document.forms[2].style.display == 'block'){
@@ -55,46 +61,134 @@ for(let fechar of botaoSair){
 
 function TransicaoCliente(posicao){
     let form = document.forms[0];
-    form[2].value = clientes[posicao-1]["codCliente"]
-    form[3].value = clientes[posicao-1]["nomeCliente"]
-    form[4].value = clientes[posicao-1]["dataCadCli"]
-    //.lenght -1 para pegar valor máximo e assim fazer incremento para botão novo
+    if(posicao===-1){
+        form[2].value = clientes.length+1
+        form[3].value = ''
+        form[4].value = getDate()
+    }
+    else{
+        if(posicao > clientes.length || posicao < 1){
+            alert('Fim da lista')
+        }
+        else{
+            console.log(posicao)
+            form[2].value = clientes[posicao - 1]["codCliente"]
+            form[3].value = clientes[posicao - 1]["nomeCliente"]
+            form[4].value = clientes[posicao - 1]["dataCadCli"]
+        }
+    }
+}
+
+function getDate(){
+    let data = new Date()
+    return `${data.getDay()}/${data.getMonth()+1}/${data.getFullYear()}`
 }
 
 function TransicaoProdutos(posicao){
     let form = document.forms[1];
-    form[2].value = produtos[posicao-1]["codProduto"]
-    form[3].value = produtos[posicao-1]["descProduto"]
-    form[4].value = produtos[posicao-1]["precoProduto"]
-    form[5].value = produtos[posicao-1]["qtdEstoqueProd"]
+    if(posicao === -1){
+        form[2].value = produtos.length+1
+        form[3].value = ''
+        form[4].value = ''
+        form[5].value = ''
+    } else {
+        if(posicao > produtos.length || posicao < 1){
+            alert('Fim da lista')
+        }
+    }
+    form[2].value = produtos[posicao - 1]["codProduto"]
+    form[3].value = produtos[posicao - 1]["descProduto"]
+    form[4].value = produtos[posicao - 1]["precoProduto"]
+    form[5].value = produtos[posicao - 1]["qtdEstoqueProd"]
 }
+
 
 
 for(let botao of botaoMov){
     botao.addEventListener('click', (mover) =>{
 
         let pos = mover.target.form[2].value
+        
 
-        if(mover.target.id == 'btnAnterior'){
-            TransicaoCliente(Number(pos) -1)
-        } else if(mover.target.id == 'btnProximo') {
-            TransicaoCliente(Number(pos)+1)
+        if(mover.target.id == 'antCliente'){
+            TransicaoCliente(Number(pos) - 1)
+            
+        } else if(mover.target.id == 'proxCliente') {
+            TransicaoCliente(Number(pos) +1)
+        } else if (mover.target.id == 'novoCliente'){
+            TransicaoCliente(-1)
+        } else if (mover.target.id == 'salvarCliente'){
+            if(document.forms[0][2].value == clientes.length+1 && document.forms[0][3].value != ''){
+                clientes.push(
+                    {
+                        "codCliente"   : document.forms[0][2].value,
+                        "nomeCliente"  : document.forms[0][3].value,
+                        "dataCadCli" : document.forms[0][4].value,
+                    }
+                )
+                alert('Dados salvos com sucesso')
+            }
+            else{
+                alert('Por favor preencher os dados')
+            }
         }
     })
 }
 
 for(let botao of botaoMov){
-    botao.addEventListener('click', (mover) =>{
-
+    botao.addEventListener('click', (mover)=>{
         let pos = mover.target.form[2].value
 
-        if(mover.target.id == 'btnAnterior'){
-            TransicaoProdutos(Number(pos) -1)
-        } else if(mover.target.id == 'btnProximo') {
-            TransicaoProdutos(Number(pos)+1)
-        } 
+        if(mover.target.id == 'antProd'){
+            TransicaoProdutos(Number(pos) - 1)
+        } else if (mover.target.id == 'proxProd') {
+            TransicaoProdutos(Number(pos) + 1)
+        } else if(mover.target.id == 'novoProd'){
+            TransicaoProdutos(-1)
+        } else if (mover.target.id == 'salvarProd'){
+            if(document.forms[1][2].value == produtos.length+1 && document.forms[1][3].value && document.forms[1][4].value && document.forms[1][5].value != ''){
+                produtos.push(        
+                    {
+                    "codProduto"   : document.forms[1][2].value,
+                    "descProduto"    : document.forms[1][3].value,
+                    "precoProduto" : document.forms[1][4].value, 
+                    "qtdEstoqueProd" : document.forms[1][5].value,   
+                  },  
+          )
+          alert('Dados salvo com sucesso')
+            } else {
+                alert('Por favor preencher os dados')
+            }
+        }
     })
 }
+
+
+
+
+// for(let botao of botaoMov){
+//     botao.addEventListener('click', (mover) =>{
+
+//         let pos = mover.target.form[2].value
+//         let ultimo = (produtos.length + 1)
+
+//         if(mover.target.id == 'antProd'){
+//             TransicaoProdutos(Number(pos) - 1)
+            
+//         } else if(mover.target.id == 'proxProd') {
+//             TransicaoProdutos(Number(pos) + 1)
+//         } else if(mover.target.id == 'novoProd') {
+//             TransicaoProdutos(ultimo)
+//         } 
+//     })
+// }
+
+
+
+
+
+
+
 
 
 
