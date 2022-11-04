dcd8f0f6e9a73a7460059d3a2f49189205c8d4eb
Douglas Menchon <114234741+Dougmgm@users.noreply.github.com>
2022-11-04 14:15:18 -0300

    Update loginscript.js

--- Desafio JS/script/loginscript.js
+++ Desafio JS/script/loginscript.js
@@ -4,59 +4,73 @@ import { produtos } from "/HTML/Desafio/module/produtos.js"
 const botaoEsquerda = document.querySelectorAll('.esquerda');
 const botaoSair = document.querySelectorAll('.sair');
 const botaoMov = document.querySelectorAll('.btnAjuste');
-const fecharPed = document.querySelectorAll('.preenchimento')
+const fecharPedCliente = document.querySelectorAll('.direitaPed');
+const codClientePed = document.querySelector("#codClientePed")
+const nomeClientePed = document.querySelector("#nomeClientePed")
+const codigoPedido = document.querySelector("#codigoPedido")
+const fecharPedProd = document.querySelector('#lançarPedido');
+
 
 TransicaoCliente(1);
 TransicaoProdutos(1);
 
-for(let botao of botaoEsquerda){    
-    botao.addEventListener('click', (event) => {
-
-        if(event.target.id == 'primeiro'){
-            3
-            if(document.forms[0].style.display == 'block'){
-                document.forms[0].style.display = 'none';
-                document.forms[1].style.display = 'none';
-                document.forms[2].style.display = 'none';
+function selecionarTela(){
+    for(let botao of botaoEsquerda){    
+        botao.addEventListener('click', (event) => {
+    
+            if(event.target.id == 'primeiro'){
+                3
+                if(document.forms[0].style.display == 'block'){
+                    document.forms[0].style.display = 'none';
+                    document.forms[1].style.display = 'none';
+                    document.forms[2].style.display = 'none';
+                }
+                else{
+                    document.forms[0].style.display = 'block'
+                    document.forms[1].style.display = 'none';
+                    document.forms[2].style.display = 'none';
+                    
+                }
+            } else if(event.target.id =='segundo'){
+                if(document.forms[1].style.display == 'block'){
+                    document.forms[1].style.display = 'none';
+                    document.forms[0].style.display = 'none';
+                    document.forms[2].style.display = 'none';
+                }
+                else{   
+                    document.forms[1].style.display = 'block';
+                    document.forms[0].style.display = 'none';
+                    document.forms[2].style.display = 'none';
+                    
+                }
+            } else{
+                if(document.forms[2].style.display == 'block'){
+                    document.forms[2].style.display = 'none';
+                    document.forms[1].style.display = 'none';
+                    document.forms[0].style.display = 'none';
+                }
+                else{
+                    document.forms[2].style.display = 'block';
+                    document.forms[1].style.display = 'none';
+                    document.forms[0].style.display = 'none';
+                }
             }
-            else{
-                document.forms[0].style.display = 'block'
-                document.forms[1].style.display = 'none';
-                document.forms[2].style.display = 'none';
-                
-            }
-        } else if(event.target.id =='segundo'){
-            if(document.forms[1].style.display == 'block'){
-                document.forms[1].style.display = 'none';
-                document.forms[0].style.display = 'none';
-                document.forms[2].style.display = 'none';
-            }
-            else{   
-                document.forms[1].style.display = 'block';
-                document.forms[0].style.display = 'none';
-                document.forms[2].style.display = 'none';
-                
-            }
-        } else{
-            if(document.forms[2].style.display == 'block'){
-                document.forms[2].style.display = 'none';
-                document.forms[1].style.display = 'none';
-                document.forms[0].style.display = 'none';
-            }
-            else{
-                document.forms[2].style.display = 'block';
-                document.forms[1].style.display = 'none';
-                document.forms[0].style.display = 'none';
-            }
-        }
-    })
+        })
+    }
+    
 }
 
-for(let fechar of botaoSair){
-    fechar.addEventListener('click', (event) => {
-        event.target.form.style.display = 'none';
-    })
+selecionarTela()
+
+function sairTela(){
+    for(let fechar of botaoSair){
+        fechar.addEventListener('click', (event) => {
+            event.target.form.style.display = 'none';
+        })
+    }
 }
+
+sairTela()
 
 function TransicaoCliente(posicao){
 
@@ -105,107 +119,181 @@ function TransicaoProdutos(posicao){
     form[5].value = produtos[posicao - 1]["qtdEstoqueProd"]
 }
 
-for(let botao of botaoMov){
-    botao.addEventListener('click', (mover) =>{
-
-        let pos = mover.target.form[2].value
-        
-
-        if(mover.target.id == 'antCliente'){
-            TransicaoCliente(Number(pos) - 1)
-        } else if(mover.target.id == 'proxCliente') {
-            TransicaoCliente(Number(pos) + 1)
-        } else if (mover.target.id == 'novoCliente'){
-            TransicaoCliente(-1)
-        } else if (mover.target.id == 'salvarCliente'){
-            if(document.forms[0][2].value == clientes.length + 1 && document.forms[0][3].value != ''){
-                clientes.push(
-                    {
-                        "codCliente"   : document.forms[0][2].value,
-                        "nomeCliente"  : document.forms[0][3].value,
-                        "dataCadCli" : document.forms[0][4].value,
-                    }
-                )
-                alert('Dados salvos com sucesso')
-            }
-            else{
-                alert('Por favor preencher os dados')
-            }
-        }
-    })
-}
-
-for(let botao of botaoMov){
-    botao.addEventListener('click', (mover)=>{
-
-        let pos = mover.target.form[2].value
-
-        if(mover.target.id == 'antProd'){
-            TransicaoProdutos(Number(pos) - 1)
-        } else if (mover.target.id == 'proxProd') {
-            TransicaoProdutos(Number(pos) + 1)
-        } else if(mover.target.id == 'novoProd'){
-            TransicaoProdutos(-1)
-        } else if (mover.target.id == 'salvarProd'){
-            if(document.forms[1][2].value == produtos.length+1 && document.forms[1][3].value && document.forms[1][4].value && document.forms[1][5].value != ''){
-                produtos.push(        
-                    {
-                    "codProduto"   : document.forms[1][2].value,
-                    "descProduto"    : document.forms[1][3].value,
-                    "precoProduto" : document.forms[1][4].value, 
-                    "qtdEstoqueProd" : document.forms[1][5].value,   
-                  },  
-          )
-          alert('Dados salvo com sucesso')
-            } else {
-                alert('Por favor preencher os dados')
-            }
-        }
-    })
-}
-
-
-for(puxar of fecharPed){
-    puxar.addEventListener('click', ()=>{
-
-        const clientesCodPed = clientes["codCliente"]
-        const clientesNomePed = clientes["nomeCliente"]
+function movCliente(){
+    for(let botao of botaoMov){
+        botao.addEventListener('click', (mover) =>{
     
-        if(clientesCodPed == codClientePed){
-            console.log(form[2][3])
-            clientesNomePed == nomeClientePed
-        } else {
-            alert('Favor, colocar um código valido')
-        }
-    })
+            let pos = mover.target.form[2].value
+            
+    
+            if(mover.target.id == 'antCliente'){
+                TransicaoCliente(Number(pos) - 1)
+            } else if(mover.target.id == 'proxCliente') {
+                TransicaoCliente(Number(pos) + 1)
+            } else if (mover.target.id == 'novoCliente'){
+                TransicaoCliente(-1)
+            } else if (mover.target.id == 'salvarCliente'){
+                if(document.forms[0][2].value == clientes.length + 1 && document.forms[0][3].value != ''){
+                    clientes.push(
+                        {
+                            "codCliente"   : document.forms[0][2].value,
+                            "nomeCliente"  : document.forms[0][3].value,
+                            "dataCadCli" : document.forms[0][4].value,
+                        }
+                    )
+                    alert('Dados salvos com sucesso')
+                }
+                else{
+                    alert('Por favor preencher os dados')
+                }
+            }
+        })
+    }
+    
 }
 
+movCliente()
 
-for(let botao of fecharPedCliente){
-    botao.addEventListener("focusout", ()=>{
 
-        let x = codClientePed.value
-
-        if(codClientePed.value == clientes[x - 1]["codCliente"]){
-        nomeClientePed.value = clientes[x - 1]["nomeCliente"]
-        } else if(codClientePed.value > clientes.length || codClientePed.value < clientes.length) {
-            alert('Favor colocar um código válido')
-        }
-    })
+function movProduto(){
+    for(let botao of botaoMov){
+        botao.addEventListener('click', (mover)=>{
+    
+            let pos = mover.target.form[2].value
+    
+            if(mover.target.id == 'antProd'){
+                TransicaoProdutos(Number(pos) - 1)
+            } else if (mover.target.id == 'proxProd') {
+                TransicaoProdutos(Number(pos) + 1)
+            } else if(mover.target.id == 'novoProd'){
+                TransicaoProdutos(-1)
+            } else if (mover.target.id == 'salvarProd'){
+                if(document.forms[1][2].value == produtos.length+1 && document.forms[1][3].value && document.forms[1][4].value && document.forms[1][5].value != ''){
+                    produtos.push(        
+                        {
+                        "codProduto"   : document.forms[1][2].value,
+                        "descProduto"    : document.forms[1][3].value,
+                        "precoProduto" : document.forms[1][4].value, 
+                        "qtdEstoqueProd" : document.forms[1][5].value,   
+                      },  
+              )
+              alert('Dados salvo com sucesso')
+                } else {
+                    alert('Por favor preencher os dados')
+                }
+            }
+        })
+    }
+    
 }
 
-for(let botao of fecharPedProd){
-    botao.addEventListener("focusout", ()=>{
+movProduto()
 
-        let x = codigoPedido.value
-
-        if(codigoPedido.value == produtos[x - 1]["codProduto"]){
-            buscarDesc.value = produtos[x - 1]["descProduto"]
-            buscarPreco.value = produtos[x - 1]["precoProduto"]
-            buscarQtde.value = produtos[x - 1]["qtdEstoqueProd"]
-        }
-    })
+function limpaClientes(){
+    codClientePed.value = ''
+    nomeClientePed.value = ''
+    codigoPedido.value = ''
+    buscarPreco.value = ''
+    buscarDesc.value = ''
+    buscarQtde.value = ''
 }
+
+function limpaPedidos(){
+    buscarQtde.value = ''
+}
+
+function getData(){
+    for(let botao of fecharPedCliente){
+        botao.addEventListener("focusout", ()=>{
+    
+            
+            let x = Number(codClientePed.value)
+    
+            if(Number(codClientePed.value) > clientes.length){
+                alert("Favor colocar um código válido")
+                limpaClientes()
+            }else{
+                x == clientes[x - 1]["codCliente"]
+                nomeClientePed.value = clientes[x - 1]["nomeCliente"]
+            }
+        })
+
+        botao.addEventListener("focusout", ()=>{
+    
+            if(Number(codigoPedido.value) > produtos.length){
+                alert("Favor colocar um código válido")
+                limpaPedidos()
+            }
+
+            let y = Number(codigoPedido.value)
+    
+            y = produtos[y - 1]["codProduto"]
+            buscarDesc.value = produtos[y - 1]["descProduto"]
+            buscarPreco.value = produtos[y - 1]["precoProduto"]
+            if(buscarQtde.value > produtos[y - 1]["qtdEstoqueProd"]){
+                alert('Favor colocar uma quantidade válida')
+                limpaPedidos()
+            }
+        })
+    }
+}
+
+getData()
+
+function organizarTabela(){
+
+    for(botao of fecharPedProd){
+        botao.addEventListener('click', ()=>{
+            
+            let tabela = document.getElementById('tabelaPed')
+            let tr = tabela.insertRow();
+        
+            let item = tr.insertCell();
+            let descricao = tr.insertCell();
+            let preco = tr.insertCell();
+            let qtd = tr.insertCell();
+            let subtotal = tr.insertCell();
+
+            item.innerText = codigoPedido.value
+            descricao.innerText = buscarDesc.value
+            preco.innerText = buscarPreco.value
+            qtd.innerText = buscarQtde.value
+            subtotal = (Number(valorQtd.value) * Number(valorPreco.value))
+            
+        })
+    }
+}
+
+organizarTabela()
+
+
+
+
+//for(let botao of fecharPedCliente){
+   // botao.addEventListener("focusout", ()=>{
+
+      //  let x = codClientePed.value
+
+     //   if(codClientePed.value == clientes[x - 1]["codCliente"]){
+     //   nomeClientePed.value = clientes[x - 1]["nomeCliente"]
+     //   } else if(codClientePed.value > clientes.length || codClientePed.value < clientes.length) {
+    //        alert('Favor colocar um código válido')
+   //     }
+ //   })
+//}
+
+//for(let botao of fecharPedProd){
+   // botao.addEventListener("focusout", ()=>{
+
+    //    let x = codigoPedido.value
+
+    //    if(codigoPedido.value == produtos[x - 1]["codProduto"]){
+    //        buscarDesc.value = produtos[x - 1]["descProduto"]
+      //      buscarPreco.value = produtos[x - 1]["precoProduto"]
+    //        buscarQtde.value = produtos[x - 1]["qtdEstoqueProd"]
+    //    }
+//    })
+//}
 
 
 
