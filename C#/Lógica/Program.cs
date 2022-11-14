f3297c6596f2dca07b771c85095bfe90bff0470b
Douglas Menchon <114234741+Dougmgm@users.noreply.github.com>
2022-11-14 14:25:45 -0300

    Add files via upload

---
+++ C#/Lógica/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+// static void Medias(){
+//     double notaIndividual = 0;
+//     double soma = 0;
+
+//     Console.WriteLine("Digite a quantidade individual de notas");
+//     int qtde = Convert.ToInt32(Console.ReadLine());
+
+//     for (int i = 1; i <= qtde; i++){
+//         Console.Write($"Digite a {i}ª nota: ");
+//         notaIndividual = Convert.ToDouble(Console.ReadLine());
+    
+//         soma = soma + notaIndividual;
+
+//     }
+
+//     double media = soma / qtde;
+
+//     if(media < 6){
+//         Console.WriteLine($"Aluno reprovado com média {media}");
+//         } else {
+//         Console.WriteLine($"Aluno aprovado com média {media}");
+//         }
+//         Console.WriteLine("Deseja calcular outra média? Aperte 1 para sim");
+
+//     int confirm = Convert.ToInt32(Console.ReadLine());
+
+//     switch(confirm){
+//         case 1: 
+//         Medias();    
+//         break;
+
+//         default:
+//         break;
+//     }
+// }
+
+// Medias();
+
---
+++ C#/POO/Program.cs
@@ -0,0 +1,16 @@
+﻿using POO.Models;
+
+Pessoa pessoa = new Pessoa();
+
+pessoa.Nome = "Douglas";
+pessoa.Idade = 24;
+
+pessoa.Apresentar();
+
+Pessoa pessoaNova = new Pessoa();
+
+pessoaNova.Nome = "Juliana";
+pessoaNova.Idade = 23;
+
+pessoaNova.Apresentar();
+
