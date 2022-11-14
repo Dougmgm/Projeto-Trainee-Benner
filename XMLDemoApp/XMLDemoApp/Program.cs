5ac4f7d1a08077f4cd7285b468b110c5ff49102e
Douglas Menchon <114234741+Dougmgm@users.noreply.github.com>
2022-11-14 14:27:41 -0300

    Add files via upload

---
+++ XMLDemoApp/XMLDemoApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml;
+
+namespace XMLDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateXMLFile();
+            System.Console.ReadLine();
+
+
+        }static void CreateXMLFile()
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlElement root = doc.CreateElement("Movies");
+            doc.AppendChild(root);
+            doc.Save(@"C:\Users\douglas.menchon\source\repos\XMLDemoApp\XMLDemoApp\Movies.xml");
+            System.Console.WriteLine(doc.InnerXml);
+        }
+    }
+}
